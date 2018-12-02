c908beef0a688f827f1a7c9a8d3b5c3d0d29bbc6
rasioatmaja29 <rasioatmaja29@gmail.com>
2018-12-02 11:54:33 +0700

    tambah interface

--- app/src/main/java/com/example/muh_r/uangkita/IHost.java
+++ app/src/main/java/com/example/muh_r/uangkita/IHost.java
@@ -3,5 +3,6 @@ package com.example.muh_r.uangkita;
 import java.util.Date;
 
 public interface IHost {
+    void openDetailBasedOnDate(Date date);
     void sendData(Date tangga_transaksil, String jenis_transaksi, String kategori_transaksi, String jumlah_transaksi, String deskripsi);
 }
