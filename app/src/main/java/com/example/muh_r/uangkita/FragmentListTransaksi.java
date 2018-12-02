6cba17457863ef30983c2a1c6661044c41a8acf7
rasioatmaja29 <rasioatmaja29@gmail.com>
2018-12-02 09:02:43 +0700

    fix bugs

--- app/src/main/java/com/example/muh_r/uangkita/FragmentListTransaksi.java
+++ app/src/main/java/com/example/muh_r/uangkita/FragmentListTransaksi.java
@@ -11,6 +11,8 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 
 /**
@@ -19,26 +21,64 @@ import java.util.ArrayList;
 public class FragmentListTransaksi extends Fragment implements View.OnClickListener {
     RecyclerView mRecyclerView;
     AdapterListTransaksi mAdapter;
-    Button mybutton;
-    ArrayList<Transaksi> transaksis = new ArrayList<>();
+
+    public Date tanggal_transaksi;
+    public String jenis_transaksi;
+    public String kategori_transaksi;
+    public String jumlah_transaksi;
+    public String deskripsi;
+
     View view;
     public FragmentListTransaksi() {
         // Required empty public constructor
     }
 
+    private static FragmentListTransaksi instance;
+    public static synchronized FragmentListTransaksi getInstance() {
+        if (instance == null) {
+            System.out.println("buat intstance");
+            instance = new FragmentListTransaksi();
+        }
+        return instance;
+    }
+
+    public static FragmentListTransaksi newInstance(Date tanggal_transaksi, String jenis_transaksi, String kategori_transaksi, String jumlah_transaksi, String deskripsi) {
+        //FragmentListTransaksi fragment = new FragmentListTransaksi();
+        FragmentListTransaksi fragment = FragmentListTransaksi.getInstance();
+        Bundle args = new Bundle();
+        args.putSerializable("tanggal", tanggal_transaksi);
+        args.putString("jenis", jenis_transaksi);
+        args.putString("kategori", kategori_transaksi);
+        args.putString("jumlah", jumlah_transaksi);
+        args.putString("deskripsi", deskripsi);
+        //args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+
+        return fragment;
+    }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            tanggal_transaksi = (Date) getArguments().getSerializable("tanggal");
+            jenis_transaksi = getArguments().getString("jenis");
+            kategori_transaksi = getArguments().getString("kategori");
+            jumlah_transaksi = getArguments().getString("jumlah");
+            deskripsi = getArguments().getString("deskripsi");
+            getArguments().clear();
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_list_transaksi, container, false);
-        Transaksi data1 = new Transaksi("Rp 150.000", "Rp 1.000.000","24 OKT");
-        Transaksi data2 = new Transaksi("Rp 50.000", "Rp 100.000","25 OKT");
-        transaksis.add(data1);
-        transaksis.add(data2);
+        if(tanggal_transaksi != null){
+            DataModel.getInstance().setListOfTransaction(new Transaksi(tanggal_transaksi,jenis_transaksi,kategori_transaksi,jumlah_transaksi,deskripsi));
+        }
 
         mRecyclerView = (RecyclerView) view.findViewById(R.id.recyclerview);
-        mAdapter = new AdapterListTransaksi(view.getContext(), transaksis);
+        mAdapter = new AdapterListTransaksi(view.getContext(), DataModel.getInstance().getListOfTransaction());
         mRecyclerView.setAdapter(mAdapter);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
         return view;
