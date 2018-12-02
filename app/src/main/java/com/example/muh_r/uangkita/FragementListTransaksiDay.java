40cf68ae6a6fa48d6c17aa46463ebd85a17264db
rasioatmaja29 <rasioatmaja29@gmail.com>
2018-12-02 10:47:38 +0700

    nambah lis per hari

---
+++ app/src/main/java/com/example/muh_r/uangkita/FragementListTransaksiDay.java
@@ -0,0 +1,79 @@
+package com.example.muh_r.uangkita;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+
+public class FragementListTransaksiDay extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    RecyclerView mRecyclerView;
+    AdapterListTransaksiDay mAdapter;
+    ArrayList<Transaksi> transaksis = new ArrayList<>();
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+    View view;
+
+    public FragementListTransaksiDay() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment FragementListTransaksiDay.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static FragementListTransaksiDay newInstance(String param1, String param2) {
+        FragementListTransaksiDay fragment = new FragementListTransaksiDay();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_fragement_list_transaksi_day, container, false);
+        Transaksi data1 = new Transaksi("Rp 150.000", "Rp 1.000.000","24 OKT");
+        Transaksi data2 = new Transaksi("Rp 50.000", "Rp 100.000","25 OKT");
+        transaksis.add(data1);
+        transaksis.add(data2);
+
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.recyclerview);
+        mAdapter = new AdapterListTransaksiDay(view.getContext(), transaksis);
+        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+        return view;
+    }
+
+}
---
+++ app/src/main/java/com/example/muh_r/uangkita/ItemListTransaksiDay.java
@@ -0,0 +1,13 @@
+package com.example.muh_r.uangkita;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ItemListTransaksiDay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_list_transaksi_day);
+    }
+}
